7d1861d26f2bd21a71cb1bf0481b3a93c07d3bf5
Minh-Huy LE <minhhuylecontact@gmail.com>
2018-04-10 23:20:56 +0200

    add feature motorbike

--- src/main/java/com/minhhuyle/gitflow/model/Vehicule.java
+++ src/main/java/com/minhhuyle/gitflow/model/Vehicule.java
@@ -3,4 +3,10 @@ package com.minhhuyle.gitflow.model;
 public abstract class Vehicule implements IVehicule {
     int x;
     int y;
+    int speed;
+
+    public Vehicule(int speed) {
+
+        this.speed = speed;
+    }
 }
